650a566873f3736dbe59286272a28c64aa06697a
aryan <aryanhazarika04@gmail.com>
2024-05-16 16:42:18 +0530

    initial trial

---
+++ src/lib/components/home/charizard-card.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let hovered = false;
+  </script>
+  
+  <div id="charizard" class="little-card card {hovered ? 'hovered' : ''}"></div>
+  
+  <style>
+    #charizard {
+      background-image: url("https://www.serebii.net/card/pokemondetectivepikachu/5.jpg");
+      background-position: center;
+      background-size: cover;
+    }
+  
+    .hovered {
+      transform: translate(200%, -160%) rotate(-15deg);
+    }
+  </style>
+  
