14f91a6ec8fc3fcada4ccc3fc7d3159aaaaaf971
aryan <aryanhazarika04@gmail.com>
2024-06-10 22:47:38 +0530

    Contact page Initial design

---
+++ src/routes/contact/socials.svelte
@@ -0,0 +1,23 @@
+<script>
+	import socialsData from './socialsData';
+</script>
+
+<div class="socials-div flex gap-4 items-center justify-center">
+	{#each socialsData as data}
+		<div class="{data.name} text-[gray] text-2xl hover:text-white">
+			<a href={data.url}><i class={data.icon}></i></a>
+		</div>
+	{/each}
+</div>
+
+<style>
+    .LinkedIn, .Github, .Twitter, .Instagram {
+        transition: color 0.2s;
+    }
+
+    .socials-div {
+        @media (max-width: 426px) {
+            gap: 0.5rem;
+        }
+    }
+</style>
