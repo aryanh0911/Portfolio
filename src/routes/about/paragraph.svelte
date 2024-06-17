6bcc96c86122c95e7f08c62f3748c878ac9f265d
aryan <aryanhazarika04@gmail.com>
2024-06-17 21:36:37 +0530

    changed para (about) & added fallback font

--- src/routes/about/paragraph.svelte
+++ src/routes/about/paragraph.svelte
@@ -4,15 +4,13 @@
 <section class="mt-auto mb-auto">
 	<div
 		id="para"
-		class="w-[90vw] md:w-[60vw] lg:w-[40vw] max-w-[549.44px] p-8 rounded-lg bg-[#2222229b]"
+		class="w-[90vw] md:w-[60vw] lg:w-[40vw] max-w-[549.44px] p-8 rounded-lg bg-[#2222229b] text-md md:text-lg"
 	>
-		<p class="text-slate-300 text-lg">
-			Hey there, I'm Aryan, a web-developer and a tech-enthusiast who's interested in learning the ins
-			and outs of the web and the possible ways I can implement things I've learnt into building
-			projects.
+		<p class="text-slate-300">
+			Hey there, I'm Aryan—a web developer and tech enthusiast with a passion for exploring the intricacies of the web. I love applying the knowledge I gain to build exciting projects.
 		</p>
 
-		<p class="text-slate-300 text-lg mt-3">
+		<p class="text-slate-300 mt-3">
 			Apart from seeing me spend time on my set-up trying to make sense out of all the code I write,
 			you can find me making music. I listen to all kinds of music, make covers and sometimes write
 			my own to put them on the internet.
@@ -22,11 +20,7 @@
 
 <style>
 	/* ---FONTS--- */
-	@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400;500;700;800&display=swap');
-
-	@import url('https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
-
-	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=M+PLUS+Rounded+1c&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 	#para {
 		visibility: hidden;
@@ -34,10 +28,6 @@
 	}
 
 	p {
-		/* font-family: 'M Plus Rounded 1c'; */
-		/* font-family: 'Averia Sans Libre'; */
-		font-family: 'Poppins';
-		/* font-weight: 500; */
-		/* letter-spacing: 0.1px; */
+		font-family: 'Poppins', sans-serif;
 	}
 </style>
