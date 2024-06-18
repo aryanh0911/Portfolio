78299a462bb0ffba79fdb7f60706a0aef473584c
aryan <aryanhazarika04@gmail.com>
2024-06-18 19:02:58 +0530

    removed font-based redundant code

--- src/routes/about/linkTile.svelte
+++ src/routes/about/linkTile.svelte
@@ -34,8 +34,13 @@
 	/* FONTS */
 	@import url('https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=M+PLUS+Rounded+1c&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-	p {
+	@font-face {
 		font-family: 'Poppins', sans-serif;
+		src: url('../../../static/fonts/Montserrat-VariableFont_wght.ttf');
+	}
+
+	p {
+		font-family: 'Poppins';
 		font-weight: 600;
 		letter-spacing: -0.01ch;
 		user-select: none;
